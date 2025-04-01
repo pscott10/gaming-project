1a718eda55e39b57320f81a9d030546ed4b6655c
arobinson38 <arobinson38@murraystate.edu>
2025-04-01 05:27:06 -0500

    Current Page designs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import LoginPage from './Pages/Login'
 import CreateAccount from './Pages/CreateAccount'
-import NavBar from './components/NavBar'
 import Login from './Pages/Login'
+import Profile from './Pages/Profile'
+import Sidebar from './components/Sidebar'
 
 
 const App = () => {
   return (
-    <div className= 'container'>
-      <NavBar/>
-       <CreateAccount/>
-      {/* <Login/> */}
+    <div>
+       {/* <Profile/> */}
+      {/* <CreateAccount/> */}
+      <Login/>
+      {/* <Sidebar/> */}
     </div>
   )
 }
--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import '../components/Login.css'
+import '../components/NavBar.css'
 
 const Login = () => {
     return (
+        <div className= "background-body">
+            <div className="nav-container">
+            <h1 className="logo">🎮 Gaming Edge</h1>
+            <ul>
+                <button className="active">Products</button>
+                <button>Home</button>
+                <button>About</button>
+                <button>Contact</button>
+                <button className="sign-in">Sign in</button>
+                <button className="register">Register</button>
+            </ul>
+        </div>
         <div className='login-container'>
             <div className="header">
                 <div className="text">Login</div>
@@ -22,6 +35,7 @@ const Login = () => {
             <div className="submit-container">
                 <div className="submit">Login</div>
             </div>
+        </div>
         </div>
     )
 }
