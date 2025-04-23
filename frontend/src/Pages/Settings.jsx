358837f047dd14bc9f89350d690fd326d733deb2
arobinson38 <arobinson38@murraystate.edu>
2025-04-22 21:31:32 -0500

    Edits to logout modal and settings

--- frontend/src/Pages/Settings.jsx
+++ frontend/src/Pages/Settings.jsx
@@ -4,7 +4,7 @@ import DarkMode from "../components/DarkMode";
 export default function Settings(){
     return(
         <div className='settings-container'>
-            <h1 className="welcome-header">Welcome, User!</h1>
+            <h1 className="welcome-header">Settings</h1>
             <ul>
                 <DarkMode/>
                 <span>Change Password?</span>
